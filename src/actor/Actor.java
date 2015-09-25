36b4889094bf75344b308e277705d67f0e42936f
tsia0001 <elimtsiagbey@yahoo.com>
2015-09-24 23:02:51 -0400

    Update Actor.java

--- src/actor/Actor.java
+++ src/actor/Actor.java
@@ -31,8 +31,9 @@ import util.SingletonRandom;
  * @author Elim Yao Tsiagbey and Rex Woollard
  * @version Lab Assignment 3: <i>The Hobbit Battlefield Simulator</i>
  */
-public abstract class Actor implements Serializable { // Could also be written as " public class Actor extends Object { " but no one ever does . . . why waste time typing something that isn't
-																											// required.
+public abstract class Actor implements Serializable 
+{ 	
+	// Could also be written as " public class Actor extends Object { " but no one ever does . . . why waste time typing something that isn't																							// required.
 	// Series of constants which are common to all objects. No instances of these values reside in any Actor objects.
 	// The keyword "static" makes a single item (such as MAX_STRENGTH) common to all Actor objects. The keyword "final" makes an item constant.
 
@@ -52,19 +53,19 @@ public abstract class Actor implements Serializable { // Could also be written a
 	private SimpleStringProperty name = new SimpleStringProperty();
 
 	/** captures a reference-to-String value stored in local, stack-oriented local variable <i>name</i>, stores in heap-oriented instance variable <i>this.name</i> */
-	public void setName(String name) {
+	public void setName(String name) 
+	{
 		this.name.set(name);
 	}
 
 	/** @return a copy of the reference-to-String value stored in <i>name</i>; safe to return because String is immutable */
-	public String getName() {
+	public String getName() 
+	{
 		return name.get();
 	}
 
 	protected Army armyAllegiance;
-
 	private TranslateTransition tt;
-
 	private Tooltip tooltip;
 
 	/** Upper boundary on <i>strength</i> attribute, currently:{@value} */
@@ -79,7 +80,8 @@ public abstract class Actor implements Serializable { // Could also be written a
 	 * 
 	 * */
 
-	public void setStrength(double strength) {
+	public void setStrength(double strength) 
+	{
 		if (strength < MIN_STRENGTH)
 			strength = MIN_STRENGTH;
 		else if (strength > MAX_STRENGTH)
@@ -88,7 +90,8 @@ public abstract class Actor implements Serializable { // Could also be written a
 	}
 
 	/** @return a copy of the <i>double</i> value stored in <i>strength</i> */
-	public double getStrength() {
+	public double getStrength() 
+	{
 		return strength.get();
 	}
 
@@ -102,7 +105,8 @@ public abstract class Actor implements Serializable { // Could also be written a
 	protected SimpleDoubleProperty health = new SimpleDoubleProperty();
 
 	/** @param <i>health</i> captures a value (within a guaranteed range) in the field <i>health</i> */
-	public void setHealth(double health) {
+	public void setHealth(double health) 
+	{
 		if (health < MIN_HEALTH)
 			health = MIN_HEALTH;
 		else if (health > MAX_HEALTH)
@@ -115,7 +119,8 @@ public abstract class Actor implements Serializable { // Could also be written a
 	protected abstract void adjustHealth(double changeToValue);
 
 	/** @return a copy of the <i>double</i> value stored in <i>health</i> */
-	public double getHealth() {
+	public double getHealth() 
+	{
 		return health.get();
 	}
 
@@ -127,7 +132,8 @@ public abstract class Actor implements Serializable { // Could also be written a
 	private SimpleDoubleProperty speed = new SimpleDoubleProperty();
 
 	/** @param <i>speed</i>captures a value (within a guaranteed range) in the field <i>speed</i> */
-	public void setSpeed(double speed) {
+	public void setSpeed(double speed) 
+	{
 		if (speed < MIN_SPEED)
 			speed = MIN_SPEED;
 		else if (speed > MAX_SPEED)
@@ -136,7 +142,8 @@ public abstract class Actor implements Serializable { // Could also be written a
 	} // end setSpeed()
 
 	/** @return a copy of the <i>double</i> value stored in <i>speed</i> */
-	public double getSpeed() {
+	public double getSpeed()
+	{
 		return speed.get();
 	}
 
@@ -144,7 +151,8 @@ public abstract class Actor implements Serializable { // Could also be written a
 	 * <i>Actor</i> constructor is used when building <i>Actor</i> objects automatically: <i>strength</i>, <i>health</i>, <i>speed</i> fields are given randomly generated values within their range;
 	 * <i>name</i> is given a sequentially numbered name: <i>Auto:<b>n</b></i> where <i><b>n</b></i> is the sequence number. The <i>name</i> can be edited to create an unique <i>Actor</i>.
 	 */
-	public Actor() {
+	public Actor() 
+	{
 		++actorSerialNumber; // static class-oriented variable. There is one-and-only-one instance of this variable regardless of the number of Actor objects in existence (from none to infinity).
 		// setName("Actor:" + actorSerialNumber);
 		setName(String.format("%d:%s", actorSerialNumber, getClass().getSimpleName())); // An alternate way to assemble a String to use as a name. Because of polymorphism "getClass().getName()" will
@@ -158,17 +166,20 @@ public abstract class Actor implements Serializable { // Could also be written a
 
 	} // end Actor constructor
 
-	public Actor(Army armyAllegiance) {
+	public Actor(Army armyAllegiance)
+	{
 		this();
 		this.armyAllegiance = armyAllegiance;
 	}
 
-	public void setArmyAllegiance(Army army) {
+	public void setArmyAllegiance(Army army)
+	{
 		armyAllegiance = army;
 
 	}
 
-	public Army getAllegiance() {
+	public Army getAllegiance() 
+	{
 		return armyAllegiance;
 	}
 
@@ -176,21 +187,23 @@ public abstract class Actor implements Serializable { // Could also be written a
 	 * sets all <i>Actor</i> fields, guaranteeing values within the specified range. Later, it will be treated as a virtual method, and subclasses will call this (the superclass method) to perform its
 	 * work.
 	 */
-	public void inputAllFields() {
+	public void inputAllFields() 
+	{
 		setName(Input.instance.getString(getClass().getSimpleName() + ":Current Name:" + name + " New Name:"));
 		setStrength(Input.instance.getDouble(String.format("Strength:%.1f", strength), MIN_STRENGTH, MAX_STRENGTH));
 		setHealth(Input.instance.getDouble(String.format("Health:%.1f", health), MIN_HEALTH, MAX_HEALTH));
 		setSpeed(Input.instance.getDouble(String.format("Speed:%.1f", speed), MIN_SPEED, MAX_SPEED));
-
 	} // end void inputAllFields()
 
 	/** <i>Actor</i> regain health on each cycle of the simulation (and loose health in battles handled by other code). */
-	public void gameCycleHealthGain() {
+	public void gameCycleHealthGain() 
+	{
 		final double MAX_CYCLE_HEALTH_GAIN = 2.0;
 		adjustHealth(Math.random() * MAX_CYCLE_HEALTH_GAIN);
 	}
 
-	public double getHitPoints() {
+	public double getHitPoints()
+	{
 		return getStrength() + getHealth() * .5 * Math.random();
 	}
 
@@ -222,7 +235,8 @@ public abstract class Actor implements Serializable { // Could also be written a
 	 * @param <i>defender</i> a reference to a different <i>Actor</i> object that will engage in combat with this <i>Actor</i> object.
 	 * @return <i>health</i> of the <i>Actor</i> following the combat round.
 	 * */
-	public double combatRound(Actor defender) {
+	public double combatRound(Actor defender) 
+	{
 		final double MAX_COMBAT_HEALTH_REDUCTION_OF_LOOSER = 10.0; // health ranges 0.0 to 100.0, thus could loose 0.0 to 10.0
 		final double MAX_COMBAT_HEALTH_REDUCTION_OF_WINNER = 3.0; // could loose 0.0 to 3.0
 		double healthAdjustmentOfLooser = -(Math.random() * MAX_COMBAT_HEALTH_REDUCTION_OF_LOOSER) - 1.0; // looser looses at least 1.0
@@ -232,7 +246,9 @@ public abstract class Actor implements Serializable { // Could also be written a
 		if (Math.random() > proportionHitPoints) {
 			adjustHealth(healthAdjustmentOfLooser);
 			defender.adjustHealth(healthAdjustmentOfWinner);
-		} else {
+		} 
+		else 
+		{
 			defender.adjustHealth(healthAdjustmentOfLooser);
 			adjustHealth(healthAdjustmentOfWinner);
 		}
@@ -244,19 +260,20 @@ public abstract class Actor implements Serializable { // Could also be written a
 	 * 
 	 * @return boolean value representing whether the <i>Actor</i> can move.
 	 * */
-	public boolean isHealthyEnoughToMove() {
+	public boolean isHealthyEnoughToMove()
+	{
 		return (getHealth() > MAX_HEALTH * THRESHOLD_OF_ADEQUATE_HEALTH);
 	} // expression results in a boolean value
 
 	/** overrides the superclass (<i>Object</i>) version of <i>toString()</i> and provides a textual representation of the <i>Actor</i> object. */
 	@Override
-	public String toString() {
+	public String toString() 
+	{
 		return String.format("Name:%-10s Health:%4.1f Strength:%4.1f Speed:%4.1f", getName(), getHealth(), getStrength(), getSpeed());
 		// return name + " Strength:" + strength + " Speed:"+ speed + " Health:" + health; // it works but its BAD FORM. Many extra String objects are created and thrown away. No field width control.
 	} // end String toString()
 
 	public abstract void createAvatar();
-
 	public abstract Node getAvatar();
 
 	/**
@@ -266,35 +283,33 @@ public abstract class Actor implements Serializable { // Could also be written a
 	 * @param engageInCombat
 	 *          TODO
 	 */
-	public void startMotion(boolean engageInCombat) {
+	public void startMotion(boolean engageInCombat) 
+	{
 		Army opposingArmy = armyAllegiance.getOpposingArmy();
 		Actor opponent = opposingArmy.findNearestOpponent(this);
 		final double DISTANCE_WHERE_WE_ENGAGE_IN_COMBAT = 50.0;
-		if (opponent != null) {
-			if (engageInCombat && distanceTo(opponent) < DISTANCE_WHERE_WE_ENGAGE_IN_COMBAT) {
+		if (opponent != null) 
+		{
+			if (engageInCombat && distanceTo(opponent) < DISTANCE_WHERE_WE_ENGAGE_IN_COMBAT) 
+			{
 				combatRound(opponent);
-				if (getHealth() <= 0.0) {
+				if (getHealth() <= 0.0)
+				{
 					armyAllegiance.removeNowDeadActor(this);
 					return; // I am now dead, thus, I cannot move . . . having been removed
 				}
 	
-				if (opponent.getHealth() <= 0.0) {
+				if (opponent.getHealth() <= 0.0)
+				{
 					opponent.armyAllegiance.removeNowDeadActor(opponent);
 					opponent = null;
 				}
 			}
-			// System.out.printf("ToMove:[%.1f:%.1f] Opponent:[%.1f:%.1f]\n",
-			// getAvatar().getTranslateX(),
-			// getAvatar().getTranslateY(),
-			// opponent.getAvatar().getTranslateX(),
-			// opponent.getAvatar().getTranslateX());
-			// *************************** Start Your work for findNewLocation() *******************************
-			// You must implement the findNewLocation() methods for each subclass
-			// The newly created Point2D object will contain the newly calculated X/Y values
-			
+
 			Point2D newLocation = armyAllegiance.validateCoordinate(findNewLocation(opponent)); // opponent MIGHT be null
 			// *************************** End Your work for findNewLocation() *******************************
-			if (tt.getStatus() != Animation.Status.RUNNING) {
+			if (tt.getStatus() != Animation.Status.RUNNING)
+			{
 				// *************************** Start Your work for to use newLocation *******************************
 				tt.setToX(newLocation.getX());
 				tt.setToY(newLocation.getY());
@@ -314,13 +329,15 @@ public abstract class Actor implements Serializable { // Could also be written a
 
 	protected abstract Point2D findNewLocation(Actor opponent);
 
-	public void pauseMotion() {
+	public void pauseMotion() 
+	{
 		if (tt != null && tt.getStatus() == Animation.Status.RUNNING)
 			tt.pause();
 	}
 
 	/** createTable is static to allow Army to define a table without having any Actor objects present. */
-	public static TableView<Actor> createTable() {
+	public static TableView<Actor> createTable() 
+	{
 		TableView<Actor> table = new TableView<Actor>();
 		final double PREF_WIDTH_DOUBLE = 50.0;
 		table.setPrefWidth(PREF_WIDTH_DOUBLE * 7.5); // 7.0 because there are 6 individual columns, but one of those is DOUBLE-WIDTH, and there is some inter-column spacing
@@ -357,28 +374,32 @@ public abstract class Actor implements Serializable { // Could also be written a
 		// The following code makes each cell in the selected columns editable (Name, Health, Strength, Speed)
 		// We CANNOT implement edit capabilities on the X/Y columns since they are READ-ONLY.
 		nameCol.setCellFactory(TextFieldTableCell.<Actor> forTableColumn());
-		nameCol.setOnEditCommit(event -> {
+		nameCol.setOnEditCommit(event ->
+		{
 			Actor a = (event.getTableView().getItems().get(event.getTablePosition().getRow()));
 			a.setName(event.getNewValue());
 			a.resetAvatarAttribute();
 		});
 
 		healthCol.setCellFactory(TextFieldTableCell.<Actor, Number> forTableColumn(new NumberStringConverter()));
-		healthCol.setOnEditCommit(event -> {
+		healthCol.setOnEditCommit(event -> 
+		{
 			Actor a = (event.getTableView().getItems().get(event.getTablePosition().getRow()));
 			a.setHealth((Double) event.getNewValue());
 			a.resetAvatarAttribute();
 		});
 
 		strengthCol.setCellFactory(TextFieldTableCell.<Actor, Number> forTableColumn(new NumberStringConverter()));
-		strengthCol.setOnEditCommit(event -> {
+		strengthCol.setOnEditCommit(event -> 
+		{
 			Actor a = (event.getTableView().getItems().get(event.getTablePosition().getRow()));
 			a.setStrength((Double) event.getNewValue());
 			a.resetAvatarAttribute();
 		});
 
 		speedCol.setCellFactory(TextFieldTableCell.<Actor, Number> forTableColumn(new NumberStringConverter()));
-		speedCol.setOnEditCommit(event -> {
+		speedCol.setOnEditCommit(event ->
+		{
 			Actor a = (event.getTableView().getItems().get(event.getTablePosition().getRow()));
 			a.setSpeed((Double) event.getNewValue());
 			a.resetAvatarAttribute();
@@ -387,7 +408,8 @@ public abstract class Actor implements Serializable { // Could also be written a
 		return table;
 	} // end createTable()
 
-	public void resetAvatarAttribute() {
+	public void resetAvatarAttribute() 
+	{
 		Tooltip.install(getAvatar(), new Tooltip(toString()));
 		Tooltip tooltip = new Tooltip();
 		Node avatar = getAvatar();
@@ -398,7 +420,8 @@ public abstract class Actor implements Serializable { // Could also be written a
 	}
 
 	// Explicit implementation of writeObject, but called implicitly as a result of recursive calls to writeObject() based on Serializable interface
-	private void writeObject(ObjectOutputStream out) throws IOException {
+	private void writeObject(ObjectOutputStream out) throws IOException 
+	{
 		out.writeObject(getName()); // SimpleDoubleProperty name is NOT serializable, so I do it manually
 		out.writeDouble(getStrength()); // SimpleDoubleProperty strength is NOT serializable, so I do it manually
 		out.writeDouble(getHealth()); // SimpleDoubleProperty health is NOT serializable, so I do it manually
@@ -417,7 +440,8 @@ public abstract class Actor implements Serializable { // Could also be written a
 
 	// Explicit implementation of readObject, but called implicitly as a result
 	// of recursive calls to readObject() based on Serializable interface
-	private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+	private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException
+	{
 		name = new SimpleStringProperty((String) in.readObject());
 		strength = new SimpleDoubleProperty(in.readDouble());
 		health = new SimpleDoubleProperty(in.readDouble());
@@ -433,7 +457,8 @@ public abstract class Actor implements Serializable { // Could also be written a
 		tt.setNode(getAvatar());
 	} // end readObject() to support serialization
 
-	public double distanceTo(Actor current) {
+	public double distanceTo(Actor current) 
+	{
 		double myX = getAvatar().getTranslateX();
 		double myY = getAvatar().getTranslateY();
 
